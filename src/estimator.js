27bab5aee023a8ed0ad1915b73339d4679da6d3a
wuodmwalimu <wilsonochieng718@gmail.com>
2020-04-09 16:34:33 +0300

    changes done

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,21 @@
 const covid19ImpactEstimator = (data) => data;
+const input  * data;
+return { 
+	data
+	{
+	region: {
+     name: "Africa",
+     avgAge: 19.7,
+     avgDailyIncomeInUSD: 5,
+     avgDailyIncomePopulation: 0.71
+       },
+     periodType: "days",
+     timeToElapse: 58,
+     reportedCases: 674,
+     population: 66622705,
+     totalHospitalBeds: 1380614
+      }
+     impact:{},
+     severeImpact:{}
 
 export default covid19ImpactEstimator;
